6101c3ab3a8e5817ac97e3232603edcb587d5ad6
LarryTubbs <larry.tubbs@gmail.com>
2020-01-02 11:49:12 -0600

    added program to send morse via buzzer

---
+++ Python/06_beep-morse-code.py
@@ -0,0 +1,76 @@
+#!/usr/bin/env python3
+
+import RPi.GPIO as GPIO
+import time
+from sys import version_info
+from . import morsecode as morse
+
+if version_info.major == 3:
+	raw_input = input
+
+
+# Set #17 as buzzer pin
+BeepPin = 17
+
+def print_message():
+	print ("========================================")
+	print ("|                 Beep                 |")
+	print ("|    ------------------------------    |")
+	print ("|        Buzzer connect to GPIO17      |")
+	print ("|                                      |")
+	print ("|            Make Buzzer beep          |")
+	print ("|                                      |")
+	print ("|                            SunFounder|")
+	print ("======================================\n")
+	print ("Program is running...")
+	print ("Please press Ctrl+C to end the program...")
+	raw_input ("Press Enter to begin\n")
+
+def setup():
+	# Set the GPIO modes to BCM Numbering
+	GPIO.setmode(GPIO.BCM)
+	# Set LedPin's mode to output, 
+	# and initial level to High(3.3v)
+	GPIO.setup(BeepPin, GPIO.OUT, initial=GPIO.HIGH)
+
+def dit():
+	print (".", end="")
+	GPIO.output(BeepPin, GPIO.LOW)
+	time.sleep(0.1)
+	GPIO.output(BeepPin, GPIO.HIGH)
+	time.sleep(0.1)
+	
+def dah():
+	print ("-", end="")
+	GPIO.output(BeepPin, GPIO.LOW)
+	time.sleep(0.1)
+	GPIO.output(BeepPin, GPIO.HIGH)
+	time.sleep(0.1)
+
+def main():
+	print_message()
+	while True:
+		msg = input("Enter message to send via morse code: ")
+		for c in msg.upper():
+			if c == ".":
+				dit()
+			elif c == "-":
+				dah()
+			elif c == " ":
+				print(c, end='')
+
+def destroy():
+	# Turn off buzzer
+	GPIO.output(BeepPin, GPIO.HIGH)
+	# Release resource
+	GPIO.cleanup()    
+
+# If run this script directly, do:
+if __name__ == '__main__':
+	setup()
+	try:
+		main()
+	# When 'Ctrl+C' is pressed, the child program 
+	# destroy() will be  executed.
+	except KeyboardInterrupt:
+		destroy()
---
+++ Python/morsecode.py
@@ -0,0 +1,57 @@
+translation_table = {"A":".-"}
+translation_table["B"] = "-..."
+translation_table["C"] = "-.-."
+translation_table["D"] = "-.."
+translation_table["E"] = "."
+translation_table["F"] = "..-."
+translation_table["G"] = "--."
+translation_table["H"] = "...."
+translation_table["I"] = ".."
+translation_table["J"] = ".---"
+translation_table["K"] = "-.-"
+translation_table["L"] = ".-.."
+translation_table["M"] = "--"
+translation_table["N"] = "-."
+translation_table["O"] = "---"
+translation_table["P"] = ".--."
+translation_table["Q"] = "--.-"
+translation_table["R"] = ".-."
+translation_table["S"] = "..."
+translation_table["T"] = "-"
+translation_table["U"] = "..-"
+translation_table["V"] = "...-"
+translation_table["W"] = ".--"
+translation_table["X"] = "-..-"
+translation_table["Y"] = "-.--"
+translation_table["Z"] = "--.."
+translation_table["1"] = ".----"
+translation_table["2"] = "..---"
+translation_table["3"] = "...--"
+translation_table["4"] = "....-	"
+translation_table["5"] = "....."
+translation_table["6"] = "-....	"
+translation_table["7"] = "--..."
+translation_table["8"] = "---.."
+translation_table["9"] = "----."
+translation_table["0"] = "-----"
+translation_table["."] = ".-.-.-"
+translation_table[","] = "--..--"
+translation_table["?"] = "..--.."
+translation_table["="] = "-...-"
+
+def translate(input):
+    morse_out = ""
+    for l in input.upper():
+        try:
+            if input != ' ':
+                morse_out += translation_table[l]
+                morse_out += " "
+        except Exception as e:
+            pass
+    return morse_out
+
+# If run this script directly, do:
+if __name__ == '__main__':
+    while True:
+        s = input("Enter text to translate: ")
+        print(translate(s))
