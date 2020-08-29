fff6d8e651cc4a08eb3eb516670c482a4fe47212
Larry Tubbs <larry.tubbs@gmail.com>
2020-08-29 11:31:15 -0500

    added lampboard code to main test

--- Python/enigma-rotor-test-classes.py
+++ Python/enigma-rotor-test-classes.py
@@ -11,6 +11,55 @@ import font
 
 GPIO.setmode(GPIO.BCM)
 
+class Lampboard():
+    def __init__(self, SDI, RCLK, SRCLK):
+        GPIO.setup(SDI, GPIO.OUT, initial=GPIO.LOW)
+        GPIO.setup(RCLK, GPIO.OUT, initial=GPIO.LOW)
+        GPIO.setup(SRCLK, GPIO.OUT, initial=GPIO.LOW)
+        self.SDI = SDI
+        self.RCLK = RCLK
+        self.SRCLK = SRCLK
+        self.segmentMap1 = {'W': 0b1, 'Q': 0b10, 'E': 0b100, 'R': 0b1000, 'T': 0b10000, 'Z': 0b100000, 'U': 0b1000000, 'I': 0b10000000, '0': 0b0}
+        self.segmentMap2 = {'O': 0b1, 'A': 0b10, 'S': 0b100, 'D': 0b1000, 'F': 0b10000, 'G': 0b100000, 'H': 0b1000000, 'J': 0b10000000, '0': 0b0}
+        self.segmentMap3 = {'K': 0b1, 'P': 0b10, 'Y': 0b100, 'X': 0b1000, 'C': 0b10000, 'V': 0b100000, 'B': 0b1000000, 'N': 0b10000000, '0': 0b0}
+        self.segmentMap4 = {'M': 0b1, 'L': 0b10, '0': 0b0}
+        self.clear()
+        
+    def clear(self):
+        self.hc595_shift(self.segmentMap4['0'])
+        self.hc595_shift(self.segmentMap3['0'])
+        self.hc595_shift(self.segmentMap2['0'])
+        self.hc595_shift(self.segmentMap1['0'])
+    
+    def hc595_shift(self, dat):
+        for bit in range(0, 8):	
+            GPIO.output(self.SDI, 0x80 & (dat << bit)) # send one bit at a time
+            GPIO.output(self.SRCLK, GPIO.HIGH) # high part of hi-low pair to signal the IC to store current bit to shift register
+            time.sleep(0.001)
+            GPIO.output(self.SRCLK, GPIO.LOW) # low part of hi-low pair to signal the IC to store current bit to shift register
+        GPIO.output(self.RCLK, GPIO.HIGH) # high part of hi-low pair to signal the IC to shift the data from the shift register to the data register
+        time.sleep(0.001)
+        GPIO.output(self.RCLK, GPIO.LOW) # low part of the hi-low pair.  Results in the value being displayed on the segment display
+
+    def show(self, c):
+        c = c.upper()
+        if c in self.segmentMap4:
+            self.hc595_shift(self.segmentMap4[c])
+        else:
+            self.hc595_shift(self.segmentMap4['0'])
+        if c in self.segmentMap3:
+            self.hc595_shift(self.segmentMap3[c])
+        else:
+            self.hc595_shift(self.segmentMap3['0'])
+        if c in self.segmentMap2:
+            self.hc595_shift(self.segmentMap2[c])
+        else:
+            self.hc595_shift(self.segmentMap2['0'])
+        if c in self.segmentMap1:
+            self.hc595_shift(self.segmentMap1[c])
+        else:
+            self.hc595_shift(self.segmentMap1['0'])
+
 class Rotary(threading.Thread):
     def __init__(self, RoAPin, RoBPin):
         threading.Thread.__init__(self)
@@ -125,7 +174,7 @@ class Display():
         GPIO.output(self.RCO, GPIO.HIGH) # breaks circuit, no ground for the right digit.
         GPIO.output(self.LCO, GPIO.HIGH)
 
-def main():
+def main(lb):
     global m3
     global stopAllThreads
     global display
@@ -136,25 +185,40 @@ def main():
     display = Display(23, 24, 25, 4, 5)
     rotary = Rotary(17, 18)
     while True:
-        name = input('Enter "Q" to quit.')
+        name = input('Enter text to encode.  Enter "-" to quit.')
 
-        if name.upper() == ('Q'):
+        if name.upper() == ('-'):
             stopAllThreads = True
             rotary.join() 
             display.clear()
-            destroy()
+            destroy(lb)
             sys.exit()
+        
+        for c in name:
+            cypherText = m3.evaluate(c)
+            display.show(enigma.globe.plaintext[m3.r.position])
+            lb.show(cypherText)
+            print(cypherText)
+            time.sleep(0.5)
+        lb.clear()
+        print(m3)
 
 
-def destroy():
+def destroy(lb=None):
+    # clear the lampboard
+    if lb is not None:
+        lb.clear()
+        
     # Release resource
     GPIO.cleanup()  
 
 # If run this script directly, do:
 if __name__ == '__main__':
+    lb = Lampboard(6, 13, 19)
+
     try:
-        main()
+        main(lb)
     # When 'Ctrl+C' is pressed, the child program 
     # destroy() will be  executed.
     except KeyboardInterrupt:
-        destroy()
+        destroy(lb)
